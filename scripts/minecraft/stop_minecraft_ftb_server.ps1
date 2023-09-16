9ec30f2276923fdc77f178738c1ccb8cf5c9927b
sjakedude <sjakedude@gmail.com>
2023-09-16 14:28:04 -0400

    syntax

--- scripts/minecraft/stop_minecraft_ftb_server.ps1
+++ scripts/minecraft/stop_minecraft_ftb_server.ps1
@@ -1,3 +1,3 @@
 $minecraft_ftb_server = gps | ? MainWindowTitle | ft Id, Name, MainWindowTitle | findstr "minecraft-ftb"
-$minecraft_pid = echo $minecraft_ftb_server.Split(" ")[1]
+$minecraft_pid = echo $minecraft_ftb_server.Split(" ")[0]
 taskkill /PID $minecraft_pid /F
--- scripts/minecraft/stop_minecraft_latest_server.ps1
+++ scripts/minecraft/stop_minecraft_latest_server.ps1
@@ -1,3 +1,3 @@
 $minecraft_latest_server = gps | ? MainWindowTitle | ft Id, Name, MainWindowTitle | findstr "minecraft-latest"
-$minecraft_pid = echo $minecraft_latest_server.Split(" ")[1]
+$minecraft_pid = echo $minecraft_latest_server.Split(" ")[0]
 taskkill /PID $minecraft_pid /F
