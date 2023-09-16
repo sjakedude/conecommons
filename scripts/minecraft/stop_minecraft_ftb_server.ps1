2669b572b9deefd5f1436c6b520cda9822e2acbc
sjakedude <sjakedude@gmail.com>
2023-09-16 14:34:02 -0400

    syntax

--- scripts/minecraft/stop_minecraft_ftb_server.ps1
+++ scripts/minecraft/stop_minecraft_ftb_server.ps1
@@ -5,3 +5,10 @@ if ( $minecraft_pid -eq "" )
     $minecraft_pid = echo $minecraft_ftb_server.Split(" ")[1]
 }
 taskkill /PID $minecraft_pid /F
+$minecraft_ftb_server = gps | ? MainWindowTitle | ft Id, Name, MainWindowTitle | findstr "minecraft-ftb"
+$minecraft_pid = echo $minecraft_ftb_server.Split(" ")[0]
+if ( $minecraft_pid -eq "" )
+{
+    $minecraft_pid = echo $minecraft_ftb_server.Split(" ")[1]
+}
+taskkill /PID $minecraft_pid /F
--- scripts/minecraft/stop_minecraft_latest_server.ps1
+++ scripts/minecraft/stop_minecraft_latest_server.ps1
@@ -3,4 +3,12 @@ $minecraft_pid = echo $minecraft_latest_server.Split(" ")[0]
 if ( $minecraft_pid -eq "" )
 {
     $minecraft_pid = echo $minecraft_latest_server.Split(" ")[1]
-}taskkill /PID $minecraft_pid /F
+}
+taskkill /PID $minecraft_pid /F
+$minecraft_latest_server = gps | ? MainWindowTitle | ft Id, Name, MainWindowTitle | findstr "minecraft-latest"
+$minecraft_pid = echo $minecraft_latest_server.Split(" ")[0]
+if ( $minecraft_pid -eq "" )
+{
+    $minecraft_pid = echo $minecraft_latest_server.Split(" ")[1]
+}
+taskkill /PID $minecraft_pid /F
