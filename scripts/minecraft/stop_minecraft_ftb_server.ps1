1cc26b62cf290809f7465385cb9a65a128c05e40
sjakedude <sjakedude@gmail.com>
2023-09-16 14:10:49 -0400

    syntax

--- scripts/minecraft/stop_minecraft_ftb_server.ps1
+++ scripts/minecraft/stop_minecraft_ftb_server.ps1
@@ -1,3 +1,3 @@
-$minecraft_ftb_server = gps | ? MainWindowTitle | ft Id, Name, MainWindowTitle | findstr "minecraft - ftb"
+$minecraft_ftb_server = gps | ? MainWindowTitle | ft Id, Name, MainWindowTitle | findstr "minecraft-ftb"
 $minecraft_pid = echo $minecraft_ftb_server.Split(" ")[1]
 taskkill /PID $minecraft_pid /F
