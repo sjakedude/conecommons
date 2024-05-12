25a058f7d169653fe4702f6029e9f0a35173c31f
Jake <sjakedude@gmail.com>
2024-05-12 11:05:43 -0400

    WIP

--- scripts/rom_sync/download_file.bat
+++ scripts/rom_sync/download_file.bat
@@ -1,7 +1,10 @@
-echo open 192.168.0.233 > download_file.txt
+echo Z: > download_file.txt
+echo cd Private/rom_saves/%1/%2 > download_file.txt
+echo open %4 > download_file.txt
 echo xboxftp >> download_file.txt
 echo xboxftp >> download_file.txt
-echo cd /Hdd1/Content/%1/%2/00000001 >> download_file.txt
-echo get %3 >> download_file.txt
+echo cd /Hdd1/Content/%3/%4/00000001 >> download_file.txt
+echo get %5 >> download_file.txt
 echo quit >> download_file.txt
+echo 
 ftp -s:download_file.txt
--- scripts/rom_sync/retrieve_metadata.bat
+++ scripts/rom_sync/retrieve_metadata.bat
@@ -1,7 +1,7 @@
-echo open 192.168.0.233 > retrieve_metadata.txt
+echo open %1 > retrieve_metadata.txt
 echo xboxftp >> retrieve_metadata.txt
 echo xboxftp >> retrieve_metadata.txt
-echo cd /Hdd1/Content/%1/%2/00000001 >> retrieve_metadata.txt
+echo cd /Hdd1/Content/%2/%3/00000001 >> retrieve_metadata.txt
 echo ls >> retrieve_metadata.txt
 echo quit >> retrieve_metadata.txt
 ftp -s:retrieve_metadata.txt
