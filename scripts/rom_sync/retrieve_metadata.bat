a90e8918aa7ff10227177477061ed8606636c69c
Jake <sjakedude@gmail.com>
2024-05-12 11:33:20 -0400

    WIP

--- scripts/rom_sync/retrieve_metadata.bat
+++ scripts/rom_sync/retrieve_metadata.bat
@@ -1,7 +1,7 @@
-echo open %1 >> retrieve_metadata.txt
-echo xboxftp >> retrieve_metadata.txt
-echo xboxftp >> retrieve_metadata.txt
+echo open %1 > retrieve_metadata.txt
+echo user xboxftp xboxftp >> retrieve_metadata.txt
 echo cd /Hdd1/Content/%2/%3/00000001 >> retrieve_metadata.txt
 echo ls >> retrieve_metadata.txt
 echo quit >> retrieve_metadata.txt
-ftp -s:retrieve_metadata.txt
+ftp -n -s:retrieve_metadata.txt
+rm retrieve_metadata.txt
