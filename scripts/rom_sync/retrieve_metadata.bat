d9ea5f3c02d407aaf9dceab733d83a697ef6e201
Jake <sjakedude@gmail.com>
2024-05-12 11:34:21 -0400

    WIP

--- scripts/rom_sync/retrieve_metadata.bat
+++ scripts/rom_sync/retrieve_metadata.bat
@@ -4,4 +4,4 @@ echo cd /Hdd1/Content/%2/%3/00000001 >> retrieve_metadata.txt
 echo ls >> retrieve_metadata.txt
 echo quit >> retrieve_metadata.txt
 ftp -n -s:retrieve_metadata.txt
-rm retrieve_metadata.txt
+del retrieve_metadata.txt
