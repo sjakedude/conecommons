78fb80ac5ec0203795246a6da4b689b2c1955bcc
Jake <sjakedude@gmail.com>
2024-05-12 13:44:20 -0400

    WIP

--- scripts/rom_sync/download_file.bat
+++ scripts/rom_sync/download_file.bat
@@ -1,8 +1,7 @@
-Z:
-cd Z:\Private\rom_saves\%5\%2\%3
 echo open %1 > download_file.txt
 echo user xboxftp xboxftp >> download_file.txt
 echo cd /Hdd1/Content/%2/%3/00000001 >> download_file.txt
+echo lcd Z:\Private\rom_saves\%5\%2\%3
 echo get %4 >> download_file.txt
 echo quit >> download_file.txt
 ftp -n -s:download_file.txt
