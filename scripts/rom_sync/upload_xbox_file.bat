66fc335ad0c0a7f530fd8349bff27002ea42011a
Jake <sjakedude@gmail.com>
2024-06-28 14:02:31 -0400

    typo

--- scripts/rom_sync/upload_xbox_file.bat
+++ scripts/rom_sync/upload_xbox_file.bat
@@ -1,7 +1,7 @@
 echo open %1 > upload_file.txt
 echo user xboxftp xboxftp >> upload_file.txt
 echo cd /Hdd1/Content/%2/%3/00000001 >> upload_file.txt
-echo lcd Z:\Private\rom_saves\%5\%2\%3 >> upload_file.txt
+echo lcd Z:\Private\rom_saves\xbox_360\%2\%3 >> upload_file.txt
 echo put %4 >> upload_file.txt
 echo quit >> upload_file.txt
 ftp -n -s:upload_file.txt
