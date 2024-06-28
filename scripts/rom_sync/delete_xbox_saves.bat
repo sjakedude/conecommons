25c9a341fec3fdd4b3a3a2acfbc376784a11e8bf
Jake <sjakedude@gmail.com>
2024-06-28 13:52:19 -0400

    adding script for deleteing xbox saves

---
+++ scripts/rom_sync/delete_xbox_saves.bat
@@ -0,0 +1,7 @@
+echo open %1 > delete_saves.txt
+echo user xboxftp xboxftp >> delete_saves.txt
+echo cd /Hdd1/Content/%2/%3/00000001 >> delete_saves.txt
+echo rm -rf %4 >> delete_saves.txt
+echo quit >> delete_saves.txt
+ftp -n -s:delete_saves.txt
+del delete_saves.txt
